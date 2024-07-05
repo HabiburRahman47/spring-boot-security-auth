2274548ca49222f97da205ef47db3d60457a2567
Habibur Rahman <habiburkuetacbd@gmail.com>
2024-07-05 15:14:40 +0600

    configure customuserDetailsService

---
+++ src/main/java/com/petproject/springsecurity/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.petproject.springsecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(email);
+        UserDetails userDetails = org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .build();
+        return userDetails;
+    }
+}
--- src/main/java/com/petproject/springsecurity/SecurityConfig.java
+++ src/main/java/com/petproject/springsecurity/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,9 +19,7 @@ public class SecurityConfig {
                         .authenticated()
                 );
         http
-                .formLogin(form->form
-                        .loginPage("/login")
-                        .permitAll());
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
 
         return http.build();
     }
